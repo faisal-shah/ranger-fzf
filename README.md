b1ddcb55e35331e9b953f5995a71e5dae135a633
Faisal Shah <faisal.shah@gmail.com>
2022-02-08 17:41:58 -0600

    Create :fzf (files and dirs), and :fzfD (dirs only) commands

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 [Ranger](https://github.com/ranger/ranger) plugin that allows navigation with [fzf](https://github.com/junegunn/fzf).
 
 Copied from the Ranger wiki [here](https://github.com/ranger/ranger/wiki/Custom-Commands) so it can be installed with [ranger-plug](https://github.com/cjbassi/ranger-plug).
+
+Forked from cjbassi's [ranger-fzf](https://github.com/cjbassi/ranger-plug) is it is no longer maintained.
+
+Modified a bit to so that :fzf is all files and dirs, and :fzfD is only dirs
--- __init__.py
+++ __init__.py
@@ -6,22 +6,37 @@ import ranger.api.commands as ranger
 
 class fzf(ranger.Command):
     """
-    :fzf_select
+    :fzf
 
-    Find a file using fzf.
+    Find a file or dir using fzf.
 
-    With a prefix argument select only directories.
     """
 
     def execute(self):
-        if self.quantifier:
-            # match only directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
-            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
-        else:
-            # match files and directories
-            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+        # match files and directories
+        command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
             -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, _ = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
+class fzfD(ranger.Command):
+    """
+    :fzfD
+
+    Find a dir using fzf.
+
+    """
+
+    def execute(self):
+        # match only directories
+        command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
         fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
         stdout, _ = fzf.communicate()
         if fzf.returncode == 0:
